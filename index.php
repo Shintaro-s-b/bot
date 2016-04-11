d2135ce7d6f519892f28ed1baf8dda8c5fba7685
shintaro <shintaro_s_b@yahoo.co.jp>
2016-04-11 22:29:54 +0900

    refactoring

--- index.php
+++ index.php
@@ -6,14 +6,11 @@ use Symfony\Component\HttpFoundation\Request;
 $app = new Silex\Application();
 
 $app->post('/callback', function (Request $request) use ($app) {
-    $client = new GuzzleHttp\Client();
-
+/*
     $body = json_decode($request->getContent(), true);
     foreach ($body['result'] as $msg) {
 
         $word = $msg['content']['text'];
-#        $api_res = file_get_contents("https://glosbe.com/gapi/translate?from=en&dest=ja&format=json&phrase=$word&pretty=true");
-#        $api_res_json = json_decode( $api_res, true );
         $api_res_json = callTranslateAPI( $word );
         $res_msg = "$word の意味は...\n";
         foreach( $api_res_json['tuc'] as $tuc ) {
@@ -23,38 +20,14 @@ $app->post('/callback', function (Request $request) use ($app) {
         }
     }
     $res_msg .= "だよっ！<(＞ ∇ ＜ )";
+*/
+    $res_msg = createMessage( $body );
 
     $resContent = $msg['content'];
     $resContent['text'] = $res_msg;
 
-/*
-    $options = [
-        'body' => json_encode([
-            'to' => [$msg['content']['from']],
-            'toChannel' => 1383378250, # Fixed value
-            'eventType' => '138311608800106203', # Fixed value
-            'content' => $resContent,
-        ]),
-        'headers' => [
-            'Content-Type' => 'application/json; charset=UTF-8',
-            'X-Line-ChannelID' => getenv('LINE_CHANNEL_ID'),
-            'X-Line-ChannelSecret' => getenv('LINE_CHANNEL_SECRET'),
-            'X-Line-Trusted-User-With-ACL' => getenv('LINE_CHANNEL_MID'),
-        ],
-        'proxy' => [
-            'https' => getenv('FIXIE_URL'),
-        ],
-    ];
-*/
     $options = createOptions( $msg, $resContent );
 
-/*
-    try {
-        $client->request('post', 'https://trialbot-api.line.me/v1/events', $requestOptions);
-    } catch (Exception $e) {
-        error_log($e->getMessage());
-    }
-*/
     sendMessage( $client, $options );
 
     return 'OKOK';
@@ -67,30 +40,46 @@ function callTranslateAPI( $word ) {
     return json_decode( $api_res, true );
 }
 
+function createMessage( $body ) {
+    foreach ( $body[ 'result' ] as $msg ) {
+        $word = $msg[ 'content' ][ 'text' ];
+        $api_res_json = callTranslateAPI( $word );
+        $res_msg = "$word の意味は...\n";
+        foreach( $api_res_json[ 'tuc' ] as $tuc ) {
+            if( !empty( $tuc[ 'phrase' ][ 'text' ] ) ) {
+                $res_msg .= $tuc[ 'phrase' ][ 'text' ] . "\n";
+            }
+        }
+    }
+    $res_msg .= "だよっ！<(＞ ∇ ＜ )";
+    return $res_msg;
+}
+
 function createOptions($msg, $resContent) {
     return $options = [
         'body' => json_encode([
-            'to' => [$msg['content']['from']],
+            'to' => [ $msg[ 'content' ][ 'from' ] ],
             'toChannel' => 1383378250, # Fixed value
             'eventType' => '138311608800106203', # Fixed value
             'content' => $resContent,
         ]),
         'headers' => [
             'Content-Type' => 'application/json; charset=UTF-8',
-            'X-Line-ChannelID' => getenv('LINE_CHANNEL_ID'),
-            'X-Line-ChannelSecret' => getenv('LINE_CHANNEL_SECRET'),
-            'X-Line-Trusted-User-With-ACL' => getenv('LINE_CHANNEL_MID'),
+            'X-Line-ChannelID' => getenv( 'LINE_CHANNEL_ID' ),
+            'X-Line-ChannelSecret' => getenv( 'LINE_CHANNEL_SECRET' ),
+            'X-Line-Trusted-User-With-ACL' => getenv( 'LINE_CHANNEL_MID' ),
         ],
         'proxy' => [
-            'https' => getenv('FIXIE_URL'),
+            'https' => getenv( 'FIXIE_URL' ),
         ],
     ];
 }
 
-function sendMessage( $client, $options ) {
+function sendMessage( $options ) {
     try {
-        $client->request('post', 'https://trialbot-api.line.me/v1/events', $options);
-    } catch (Exception $e) {
-        error_log($e->getMessage());
+        $client = new GuzzleHttp\Client()
+        $client->request( 'post', 'https://trialbot-api.line.me/v1/events', $options );
+    } catch ( Exception $e ) {
+        error_log( $e->getMessage() );
     }
 }
